65914535a9a37258612730849dd910455d3f60fb
Anders Andersson <anders.andersson@tretton37.com>
2023-12-03 20:53:54 +0100

    fix

--- src/day03/index.js
+++ src/day03/index.js
@@ -4,8 +4,7 @@ const parseInput = (rawInput) => rawInput.split("\n");
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
-  let score = 0;
-  input.forEach((line, idx) => {
+  return input.reduce((acc, line, idx) => {
     const numbersMatch = [...line.matchAll(new RegExp(/\d+/, "g"))];
     const allSymbolsMatch = [input[idx - 1], line, input[idx + 1]]
       .map((l) => {
@@ -15,20 +14,18 @@ const part1 = (rawInput) => {
     numbersMatch.forEach((m) => {
       allSymbolsMatch.forEach((sm) => {
         if (sm.index >= m.index - 1 && sm.index <= m.index + m[0].length) {
-          score += Number(m[0]);
+          acc += Number(m[0]);
         }
       });
     });
-  });
-
-  return score;
+    return acc;
+  }, 0);
 };
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
 
-  let score = 0;
-  input.forEach((line, idx) => {
+  return input.reduce((acc, line, idx) => {
     const gearMatch = [...line.matchAll(new RegExp(/\*/, "g"))];
     const allNumbersMatch = [input[idx - 1], line, input[idx + 1]]
       .map((l) => {
@@ -36,7 +33,7 @@ const part2 = (rawInput) => {
       })
       .flatMap((m) => m);
 
-    gearMatch.forEach((gear) => {
+    return (acc += gearMatch.reduce((a, gear) => {
       const nums = [];
       allNumbersMatch.forEach((num) => {
         if (
@@ -47,12 +44,11 @@ const part2 = (rawInput) => {
         }
       });
       if (nums.length === 2) {
-        score += nums[0] * nums[1];
+        return (a += nums[0] * nums[1]);
       }
-    });
-  });
-
-  return score;
+      return a;
+    }, 0));
+  }, 0);
 };
 
 const input = `467..114..
